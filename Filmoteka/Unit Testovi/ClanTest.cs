5561d6d31f6f3068f67fe323e763be2bcf393405
Avujcic1 <avujcic1@etf.unsa.ba>
2020-12-02 19:11:04 +0100

    Kostur za testove

---
+++ Filmoteka/Unit Testovi/ClanTest.cs
@@ -0,0 +1,45 @@
+﻿using Filmoteka;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Unit_Testovi
+{
+    [TestClass]
+    public class ClanTest
+    {
+        #region ProduziRok
+
+        [TestMethod]
+        public void ClanKonstruktor1()
+        {
+
+
+        }
+
+
+        [TestMethod]
+        public void ClanKonstruktor2()
+        {
+
+
+        }
+
+        [TestMethod]
+        public void vratiWatchList()
+        {
+
+
+        }
+
+        [TestMethod]
+        public void vratiRokPreplate()
+        {
+
+
+        }
+
+
+        #endregion 
+    }
+}
